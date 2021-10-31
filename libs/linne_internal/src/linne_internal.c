48acae04b12840cd3dba6dc50e38bbdc239f2403
aikiriao <samuraiaiki@gmail.com>
2021-11-01 01:11:42 +0900

    update presets.

--- libs/linne_internal/src/linne_internal.c
+++ libs/linne_internal/src/linne_internal.c
@@ -9,9 +9,9 @@
         array\
     }
 
-static const uint32_t num_params_preset1[] = {  8,  8,  8 };
-static const uint32_t num_params_preset2[] = { 16, 16, 16 };
-static const uint32_t num_params_preset3[] = { 32, 32, 32 };
+static const uint32_t num_params_preset1[] = {      4,  4 };
+static const uint32_t num_params_preset2[] = {      8,  8 };
+static const uint32_t num_params_preset3[] = {  4,  8,  8 };
 
 /* パラメータプリセット配列 */
 const struct LINNEParameterPreset g_linne_parameter_preset[LINNE_NUM_PARAMETER_PRESETS] = {
