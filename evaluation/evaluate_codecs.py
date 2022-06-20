bef0116905e0e7f5ab23d34f68d2a8f1d1bd5aca
aikiriao <samuraiaiki@gmail.com>
2022-06-20 22:17:28 +0900

    Add evaluation scripts.

---
+++ evaluation/evaluate_codecs.py
@@ -0,0 +1,220 @@
+" コーデック評価 "
+import glob
+import os
+import shutil
+from pathlib import Path
+from timeit import timeit
+import numpy as np
+import scipy.io.wavfile as wv
+import csv
+
+def _get_wavfile_length_sec(filename):
+    """ 音声ファイル長計測 """
+    rate, data = wv.read(filename)
+    return data.shape[0] / rate
+
+def _measure_execution_time(command):
+    """ 実行時間の計測 """
+    return timeit(stmt = f'subprocess.run(\'{command}\','\
+        'shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)',
+        setup = "import subprocess", number = 1)
+
+class Codec:
+    """ コーデックインターフェース """
+    def __init__(self, compress_option):
+        self.compress_option = compress_option
+
+    def get_label(self):
+        """ 実験ラベル取得 """
+        raise NotImplementedError
+
+    def generate_encode_command(self, in_filename, out_filename):
+        """ エンコードコマンド取得 """
+        raise NotImplementedError
+
+    def generate_decode_command(self, in_filename, out_filename):
+        """ デコードコマンド取得 """
+        raise NotImplementedError
+
+    def encode(self, in_filename, out_filename):
+        """ エンコード """
+        escaped_in_filename = f'\"{in_filename}\"'
+        command = self.generate_encode_command(escaped_in_filename, out_filename)
+        time = _measure_execution_time(command)
+        return (time, os.path.getsize(out_filename))
+
+    def decode(self, in_filename, out_filename):
+        """ デコード """
+        command = self.generate_decode_command(in_filename, out_filename)
+        return _measure_execution_time(command)
+
+class Flac(Codec):
+    """ FLAC """
+    def get_label(self):
+        return f"Flac {self.compress_option}"
+    def generate_encode_command(self, in_filename, out_filename):
+        return f'flac {self.compress_option} -f -s -o {out_filename} {in_filename}'
+    def generate_decode_command(self, in_filename, out_filename):
+        return f"flac -d -f -s -o {out_filename} {in_filename}"
+
+class WavPack(Codec):
+    """ WavPack """
+    def get_label(self):
+        return f"WavPack {self.compress_option}"
+    def generate_encode_command(self, in_filename, out_filename):
+        return f"wavpack {self.compress_option} -q -y {in_filename} -o {out_filename}"
+    def generate_decode_command(self, in_filename, out_filename):
+        return f"wvunpack {in_filename} -q -y -o {out_filename}"
+
+class TTA(Codec):
+    """ TTA """
+    def get_label(self):
+        return "TTA"
+    def generate_encode_command(self, in_filename, out_filename):
+        return f"tta -e {in_filename} {out_filename}"
+    def generate_decode_command(self, in_filename, out_filename):
+        return f"tta -d {in_filename} {out_filename}"
+
+class MonkeysAudio(Codec):
+    """ Monkey's Audio """
+    def get_label(self):
+        return f"Monkey's Audio {self.compress_option}"
+    def generate_encode_command(self, in_filename, out_filename):
+        return f"mac {in_filename} {out_filename} {self.compress_option}"
+    def generate_decode_command(self, in_filename, out_filename):
+        # .apeにしないと展開に失敗する...
+        in_path = Path(in_filename)
+        ape_in_filename = in_path.with_suffix('.ape')
+        os.replace(in_filename, ape_in_filename)
+        return f"mac {ape_in_filename} {out_filename} -d"
+
+class MPEG4ALS(Codec):
+    """ MPEG4-ALS """
+    def get_label(self):
+        return f"MPEG4-ALS {self.compress_option}"
+    def generate_encode_command(self, in_filename, out_filename):
+        if os.name == 'nt':
+            command = f"mp4alsRM23 {in_filename} {out_filename}"
+        else:
+            command = f"wine64 mp4alsRM23.exe {in_filename} {out_filename}"
+        return command
+    def generate_decode_command(self, in_filename, out_filename):
+        if os.name == 'nt':
+            command = f"mp4alsRM23 -x {in_filename} {out_filename}"
+        else:
+            command = f"wine64 mp4alsRM23.exe -x {in_filename} {out_filename}"
+        return command
+
+TEST_FILE_DIRECTORY_DICT = {
+       "classic": [
+           './data/RWC Music Database Sub-Working Group - Rwc-Mdb-C-2001-M*/**/*.wav'
+           ],
+       "genre": [
+           './data/RWC Music Database Sub-Working Group - Rwc-Mdb-G-2001-M*/**/*.wav'
+           ],
+       "jazz": [
+           './data/RWC Music Database Sub-Working Group - Rwc-Mdb-J-2001-M*/**/*.wav'
+           ],
+       "popular": [
+           './data/RWC Music Database Sub-Working Group - Rwc-Mdb-P-2001-M*/**/*.wav'
+           ],
+        "right": [
+            './data/RWC Music Database Sub-Working Group - Rwc-Mdb-R-2001-M*/**/*.wav'
+            ],
+        }
+
+CODEC_CONFUGURES = [
+        Flac("-5"),
+        Flac("-8"),
+        WavPack(""),
+        WavPack("-h"),
+        WavPack("-hh"),
+        TTA(""),
+        MonkeysAudio("-c2000"),
+        MonkeysAudio("-c3000"),
+        MonkeysAudio("-c4000"),
+        MPEG4ALS(""),
+    ]
+
+if __name__ == "__main__":
+    # 一時ファイル名
+    COMPRESS_TMP_FILENAME = "compressed.tmp"
+    DECOMPRESS_TMP_FILENAME = "decompressd.tmp"
+
+    filesdir = {}
+    # wavファイルリストを取得
+    for c, ds in TEST_FILE_DIRECTORY_DICT.items():
+        filesdir[c] = []
+        for d in ds:
+            filesdir[c] += glob.glob(d, recursive=True)
+        # パス文字列を統一的に扱うため'\\'を'/'に置換
+        if os.name == 'nt':
+            filesdir[c] = [f.replace('\\', '/') for f in filesdir[c]]
+        filesdir[c].sort()
+
+    # 計測
+    results = {}
+    for codec in CODEC_CONFUGURES:
+        results[codec.get_label()] = {}
+        for categ, fs in filesdir.items():
+            results[codec.get_label()][categ] = { 'encode_time': [], 'decode_time': [], 'compress_rate': [] }
+            for f in fs:
+                print(f'[{codec.get_label()}] {f}')
+                # 1ファイル計測
+                encode_time, size = codec.encode(f, COMPRESS_TMP_FILENAME)
+                decode_time = codec.decode(COMPRESS_TMP_FILENAME, DECOMPRESS_TMP_FILENAME)
+                # 結果記録
+                original_time = _get_wavfile_length_sec(f)
+                results[codec.get_label()][categ]['encode_time'].append((encode_time * 100) / original_time)
+                results[codec.get_label()][categ]['decode_time'].append((decode_time * 100) / original_time)
+                results[codec.get_label()][categ]['compress_rate'].append((size * 100) / os.path.getsize(f))
+                # 中間生成物の削除
+                if os.path.exists(COMPRESS_TMP_FILENAME):
+                    os.remove(COMPRESS_TMP_FILENAME)
+                if os.path.exists(DECOMPRESS_TMP_FILENAME):
+                    os.remove(DECOMPRESS_TMP_FILENAME)
+
+    # 全カテゴリ結果を結合した結果の計算
+    total_result = {}
+    for codec in CODEC_CONFUGURES:
+        total_result[codec] = { 'encode_time': [], 'decode_time': [], 'compress_rate': [] }
+        for categ in filesdir.keys():
+            for e in total_result[codec].keys():
+                total_result[codec][e].extend(results[codec.get_label()][categ][e])
+        for e in total_result[codec].keys():
+            total_result[codec][e] = np.mean(total_result[codec][e])
+
+    # 結果出力
+    with open('codec_comaprison_summery.csv', 'w') as f:
+        writer = csv.writer(f, lineterminator='\n')
+        header = ['']
+        for codec in CODEC_CONFUGURES:
+            header.append(codec.get_label())
+        writer.writerow(header)
+        for categ in filesdir.keys():
+            row = [f'{categ} mean encode time']
+            for codec in CODEC_CONFUGURES:
+                row.append(np.mean(results[codec.get_label()][categ]['encode_time']))
+            writer.writerow(row)
+        row = ['Total mean encode time']
+        for codec in CODEC_CONFUGURES:
+            row.append(total_result[codec]['encode_time'])
+        writer.writerow(row)
+        for categ in filesdir.keys():
+            row = [f'{categ} mean decode time']
+            for codec in CODEC_CONFUGURES:
+                row.append(np.mean(results[codec.get_label()][categ]['decode_time']))
+            writer.writerow(row)
+        row = ['Total mean decode time']
+        for codec in CODEC_CONFUGURES:
+            row.append(total_result[codec]['decode_time'])
+        writer.writerow(row)
+        for categ in filesdir.keys():
+            row = [f'{categ} mean compression rate']
+            for codec in CODEC_CONFUGURES:
+                row.append(np.mean(results[codec.get_label()][categ]['compress_rate']))
+            writer.writerow(row)
+        row = ['Total mean compression rate']
+        for codec in CODEC_CONFUGURES:
+            row.append(total_result[codec]['compress_rate'])
+        writer.writerow(row)
---
+++ evaluation/plot_total_performance_graph.py
@@ -0,0 +1,27 @@
+" コーデック評価グラフの作成 "
+import pandas as pd
+import matplotlib.pyplot as plt
+from adjustText import adjust_text
+
+CODEC_LABEL_PREFIXES = ['Flac', 'WavPack', 'TTA', 'Monkey\'s Audio', 'MPEG4-ALS']
+COLORLIST = ['r', 'g', 'b', 'c', 'm', 'y', 'k', 'w']
+
+if __name__ == "__main__":
+    df = pd.read_csv('codec_comaprison_summery.csv', index_col=0)
+    # デコード速度 v.s. 圧縮率グラフ
+    texts = []
+    for inx, cprefix in enumerate(CODEC_LABEL_PREFIXES):
+        line = [[], []]
+        for label in df.keys():
+            if label.startswith(cprefix):
+                plt.scatter(df.at['Total mean decode time', label], df.at['Total mean compression rate', label], label=label, color=COLORLIST[inx])
+                texts.append(plt.text(df.at['Total mean decode time', label], df.at['Total mean compression rate', label], label))
+                line[0].append(df.at['Total mean decode time', label])
+                line[1].append(df.at['Total mean compression rate', label])
+        if len(line[0]) > 1:
+            plt.plot(line[0], line[1], color=COLORLIST[inx])
+    adjust_text(texts)
+    plt.xlabel('Mean decoding speed (%)')
+    plt.ylabel('Mean compression rate (%)')
+    plt.grid()
+    plt.show()
