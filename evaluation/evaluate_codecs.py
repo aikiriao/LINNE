5b577afb4adcc243baa11393c7be7d4711402ba6
aikiriao <samuraiaiki@gmail.com>
2022-06-22 21:46:07 +0900

    add LINNE codec.

--- evaluation/evaluate_codecs.py
+++ evaluation/evaluate_codecs.py
@@ -105,6 +105,15 @@ class MPEG4ALS(Codec):
             command = f"wine64 mp4alsRM23.exe -x {in_filename} {out_filename}"
         return command
 
+class LINNE(Codec):
+    """ LINNE """
+    def get_label(self):
+        return f"LINNE {self.compress_option}"
+    def generate_encode_command(self, in_filename, out_filename):
+        return f"linne {self.compress_option} -e {in_filename} {out_filename}"
+    def generate_decode_command(self, in_filename, out_filename):
+        return f"linne {self.compress_option} -d {in_filename} {out_filename}"
+
 TEST_FILE_DIRECTORY_DICT = {
        "classic": [
            './data/RWC Music Database Sub-Working Group - Rwc-Mdb-C-2001-M*/**/*.wav'
